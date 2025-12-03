fc4037dac4b0c944862c4988b1d51381cf8eec94
Revolt <118598395+ImRevoltexee@users.noreply.github.com>
2025-12-03 20:55:25 +0700

    Update Discord link in libmain.lua

--- libmain.lua
+++ libmain.lua
@@ -1,6 +1,6 @@
 --// Xyra - Hub Library
 --// Founder : Revolt
---// Discord : https://discord.gg/rwGam2ZJJ
+--// Discord : https://discord.gg/rGxFUnD2
 local Library = {}
 Library.__index = Library
 Library.Async = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
@@ -210,7 +210,7 @@ function Library:SetupAboutUs(AboutUs)
         Title = "Discord Link (Click to Copy)",
         Icon = "link",
         Callback = function()
-            setclipboard("https://discord.gg/rwGam2ZJJ")
+            setclipboard("https://discord.gg/rGxFUnD2")
             Library.Async:Notify({
                 Title = "Copied!",
                 Content = "Discord link copied!",
